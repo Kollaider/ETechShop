dbc00352aa17e551fab021b7874f5dffe409ebcc
kolaider <nikolai.strom@skz.by>
2023-12-20 13:22:10 +0300

    fix: handling permissions

--- api/permissions.py
+++ api/permissions.py
@@ -2,4 +2,4 @@ from rest_framework import permissions
 
 class IsActiveEmployeePermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.employee.is_active
+        return request.user.is_authenticated and request.user.is_active
